5648fc291c5aebc126a87244b075974bec1a18a8
Johann Dee <johanndee1994@gmail.com>
2023-11-23 23:05:37 -0800

    Set up fetch

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,15 @@
 import Header from '../Header/Header';
 import AllNewNews from '../AllNewNews/AllNewNews';
+import { useState, useEffect } from 'react';
+import { fetchSportsData } from '../../apiCalls';
 
 function App() {
+
+  useEffect(() => {
+    fetchSportsData()
+    .then(data => console.log(data))
+  }, []);
+
   return (
     <>
       <Header />
